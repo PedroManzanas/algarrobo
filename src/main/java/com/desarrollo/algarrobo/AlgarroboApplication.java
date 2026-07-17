973e93994b392d1641d8b007f25e4344bc053247
Alvaro <4sedfr0n@gmail.com>
2026-07-17 00:48:08 -0300

    Proyecto Inicial

---
+++ src/main/java/com/desarrollo/algarrobo/AlgarroboApplication.java
@@ -0,0 +1,13 @@
+package com.desarrollo.algarrobo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgarroboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgarroboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desarrollo/algarrobo/AlgarroboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desarrollo.algarrobo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgarroboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
